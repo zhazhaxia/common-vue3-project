dfa8ff23714f8a91bfc4d38f2d21930e1a170a13
zhazhaxia <337262356@qq.com>
2026-03-12 20:54:42 +0800

    test: hooks

--- src/projects/test3/src/main.ts
+++ src/projects/test3/src/main.ts
@@ -5,9 +5,7 @@ import router from './router';
 
 const app = createApp(App);
 const pinia = createPinia();
-[1, 2, 3].map((item) => {
-  console.log(item);
-});
+
 app.use(router);
 app.use(pinia);
 app.mount('#app');
